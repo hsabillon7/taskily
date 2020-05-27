da15a6922ae20fdd604500907eb6c94727e994d3
Héctor Sabillón <h.hsabillon@gmail.com>
2020-05-27 08:20:59 -0600

    Creación del controlador para los proyectos. Implementación del controlador en las rutas.

---
+++ controllers/proyectosController.js
@@ -0,0 +1,7 @@
+exports.home = (req, res, next) => {
+  res.send("¡Bienvenido!");
+};
+
+exports.holaMundo = (req, res, next) => {
+  res.send("¡Hola mundo!");
+};
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@
 const express = require("express");
 const routes = express.Router();
 
+// Importar los controladores
+const proyectosController = require("../controllers/proyectosController");
+
 // Construir las rutas disponibles para el servidor
 // Las rutas deben exportarse para poder ser utilizadas en otros archivos
 module.exports = function () {
-  routes.get("/");
+  routes.get("/", proyectosController.home);
+
+  routes.get("/holamundo", proyectosController.holaMundo);
 
   return routes;
 };
