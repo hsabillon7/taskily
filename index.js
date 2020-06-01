4a83ac811172bd83d9e2455fab63c640fd4f9db2
Héctor Sabillón <h.hsabillon@gmail.com>
2020-06-01 15:55:51 -0600

    Integración del modelo en el servidor. Implementación de Handlebars como Template Engine.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Importar los módulos de express.js
 const express = require("express");
+// Importar Handlebars
+const exphbs = require("express-handlebars");
 // Importar todas las rutas disponibles
 const routes = require("./routes");
 
 // Crear la conexión con la base de datos
 const db = require("./config/db");
+
+// Importar los modelos
+require("./models/Proyecto");
 
 // Realizar la conexión a la base de datos
 // Sequelize se conecta mediante promises
@@ -15,6 +20,17 @@ db.sync()
 
 // Crear un servidor de express
 const app = express();
+
+// Indicar el template engine a utilizar (Handlebars)
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
 
 // Indicarle a express dónde están las rutas del servidor
 app.use("/", routes());
