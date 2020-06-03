0dfde9f26ff1fdc096ac422a472cde6794444316
Héctor Sabillón <h.hsabillon@gmail.com>
2020-06-02 18:53:33 -0600

    Comienzo de la funcionalidad del controlador para nuevos proyectos

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,7 +1,31 @@
+// Importar los modelos necesarios
+const Proyecto = require("../models/Proyecto");
+
+// Muestra todos los proyectos del usuario
 exports.home = (req, res, next) => {
-  res.send("¡Bienvenido!");
+  res.render("crear_proyecto");
 };
 
-exports.holaMundo = (req, res, next) => {
-  res.send("¡Hola mundo!");
+// Permite la creación de un nuevo proyecto
+exports.nuevoProyecto = (req, res, next) => {
+  // Validar que el input del formulario tenga valor
+  // Para acceder a los valores y asignarlos en un solo paso
+  // vamos a utilizar destructuring.
+  const { nombre } = req.body;
+  const errores = [];
+
+  // Verificar si el nombre del proyecto tiene un valor
+  if (!nombre) {
+    errores.push({ error: "El nombre del proyecto no puede ser vacío." });
+  }
+
+  // Si hay errores
+  if (errores.length) {
+    res.render("crear_proyecto", {
+      errores,
+    });
+  } else {
+    // Insertar el proyecto a la base de datos
+    res.send("Insertado en la BD");
+  }
 };
