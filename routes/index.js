5b144443b1314faa2c764cc3a09056c44c167d89
Héctor Sabillón <h.hsabillon@gmail.com>
2020-06-02 18:53:09 -0600

    Ruta para creación de nuevos proyectos

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ const proyectosController = require("../controllers/proyectosController");
 // Construir las rutas disponibles para el servidor
 // Las rutas deben exportarse para poder ser utilizadas en otros archivos
 module.exports = function () {
+  // Documentación sobre los distintos verbos HTTP
+  // https://developer.mozilla.org/es/docs/Web/HTTP/Methods
   routes.get("/", proyectosController.home);
 
-  routes.get("/holamundo", proyectosController.holaMundo);
+  routes.post("/nuevo_proyecto", proyectosController.nuevoProyecto);
 
   return routes;
 };
