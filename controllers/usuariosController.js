da44daeddefa98dfa18c02562700337cbc3a32cd
Héctor Sabillón <h.hsabillon@gmail.com>
2020-06-18 15:01:40 -0600

    Controlador encargado de los usuarios. Funcionalidad de creación de nuevas cuentas y de formulario de inicio de sesión

---
+++ controllers/usuariosController.js
@@ -0,0 +1,31 @@
+// Importar el modelo
+const Usuario = require("../models/Usuario");
+
+exports.formularioCrearCuenta = (req, res, next) => {
+  res.render("registrarse", { layout: "auth" });
+};
+
+exports.crearCuenta = async (req, res, next) => {
+  // Obtener los datos de la nueva cuenta
+  // Obtener por destructuring
+  const { fullname, email, password } = req.body;
+
+  // Intentar crear el usuario
+  try {
+    // Crear el usuario
+    await Usuario.create({
+      fullname,
+      email,
+      password,
+    });
+
+    // Redireccionar el usuario al formulario de inicio de sesión
+    res.redirect("iniciar_sesion");
+  } catch (error) {
+    res.render("registrarse", { layout: "auth", error });
+  }
+};
+
+exports.formularioIniciarSesion = (req, res, next) => {
+  res.render("iniciar_sesion", { layout: "auth" });
+};
