89562dc985d81552c893de77c58e7b54f4703d30
Héctor Sabillón <h.hsabillon@gmail.com>
2020-06-18 15:02:03 -0600

    Nuevas rutas para registro de usuarios e inicio de sesión

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 
 // Importar los controladores
 const proyectosController = require("../controllers/proyectosController");
+const usuariosController = require("../controllers/usuariosController");
+const authController = require("../controllers/authController");
 
 // Construir las rutas disponibles para el servidor
 // Las rutas deben exportarse para poder ser utilizadas en otros archivos
@@ -15,6 +17,16 @@ module.exports = function () {
   routes.post("/nuevo_proyecto", proyectosController.nuevoProyecto);
 
   routes.get("/nuevo_proyecto", proyectosController.formularioNuevoProyecto);
+
+  // Rutas para autenticación
+
+  routes.get("/registrate", usuariosController.formularioCrearCuenta);
+
+  routes.post("/registrate", usuariosController.crearCuenta);
+
+  routes.get("/iniciar_sesion", usuariosController.formularioIniciarSesion);
+
+  routes.post("/iniciar_sesion", authController.autenticarUsuario);
 
   return routes;
 };
