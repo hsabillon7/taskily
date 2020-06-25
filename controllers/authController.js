117beb0b569f840b37be67b4d8820228387c98e0
Héctor Sabillón <h.hsabillon@gmail.com>
2020-06-24 19:17:03 -0600

    Función que nos permite cerrar la sesión (destruyendo la sesión). Función que nos permite verificar si un usuario se encuentra o no autenticado

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,4 +10,24 @@ exports.autenticarUsuario = passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/iniciar_sesion",
   badRequestMessage: "Debes ingresar tu correo electrónico y tu contraseña",
+  failureFlash: true,
 });
+
+// Cerrar la sesión del usuario actual
+exports.cerrarSesion = (req, res, next) => {
+  // Al cerrar sesión redirigimos al usuario al inicio de sesión
+  req.session.destroy(() => {
+    res.redirect("/iniciar_sesion");
+  });
+};
+
+// Verificar si el usuario está autenticado o no
+exports.usuarioAutenticado = (req, res, next) => {
+  // Si el usuario está autenticado que continúe con la petición
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // Si el usuario no está autenticado, iniciar sesión
+  return res.redirect("/iniciar_sesion");
+};
